446d82ff7a7a8d13c1773ee238f8e0a066bd5995
Berishev Ravil <quadraticteam@gmail.com>
2021-02-25 06:08:42 +0300

    !

--- src/render/renderView.tsx
+++ src/render/renderView.tsx
@@ -4,7 +4,7 @@ import { View } from '../component';
 export function getRenderView(viewInstance: View) {
   const RenderView: React.FunctionComponent<{ [key: string]: any }> = (props) => {
     const result = viewInstance && viewInstance.render && viewInstance.render();
-    return <React.Fragment>{result || props.children}</React.Fragment>;
+    return <>{result || props.children}</>;
   };
   return RenderView;
 }
