31a090809c685b20e6d040fe42762f9772e382ad
Berishev Ravil <quadraticteam@gmail.com>
2021-09-10 21:14:28 +0300

    !

--- src/component/hoc.ts
+++ src/component/hoc.ts
@@ -2,6 +2,7 @@ import { SYMBOL_VIEW, SYMBOL_MODEL } from '../const';
 import { ControllerClass } from './controller';
 import { ViewClass } from './view';
 import { ModelFabric } from './model';
+import { Controller } from '.';
 
 export function setView<TView extends ViewClass>(viewClass: TView) {
   return function <TController extends ControllerClass>(controller: TController) {
@@ -14,8 +15,8 @@ export function getView(controller: ControllerClass): ViewClass {
   return controller && controller[SYMBOL_VIEW];
 }
 
-export function setModel<TModel extends ModelFabric>(modelClass: TModel) {
-  return function <TController extends ControllerClass>(controller: TController) {
+export function setModel<BaseController extends Controller>(modelClass: ModelFabric<BaseController>) {
+  return function <TController extends BaseController>(controller: TController) {
     controller[SYMBOL_MODEL] = modelClass;
     return controller;
   };
