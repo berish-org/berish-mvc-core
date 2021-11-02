56e9f2e27df04ef69c9e1d7f7647bf36d33bd10b
Berishev Ravil <quadraticteam@gmail.com>
2021-11-03 02:57:08 +0300

    fix remove model

--- src/withController.tsx
+++ src/withController.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Controller, ControllerClass, setView } from './component';
 
 export function withController<TProps>(Component: React.ComponentClass<TProps> | React.FunctionComponent<TProps>): ControllerClass<TProps> {
-  class HocControllerClass extends Controller<TProps, {}> {}
+  class HocControllerClass extends Controller<TProps> {}
 
   setView(Component)(HocControllerClass);
   return HocControllerClass;
